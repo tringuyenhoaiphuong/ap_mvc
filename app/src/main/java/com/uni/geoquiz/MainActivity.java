1e30ea47ddbec1b075bae331d92af8b49ba217bb
Canh Le My <lemycanh@gmail.com>
2020-11-19 09:08:41 +0700

    * Activity Life Cycle

--- app/src/main/java/com/uni/geoquiz/MainActivity.java
+++ app/src/main/java/com/uni/geoquiz/MainActivity.java
@@ -1,8 +1,10 @@
 package com.uni.geoquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -15,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "GEOQUIZ";
+    private static final String SAVED_CUR_QUESTION_IDX = "SAVED_CUR_QUESTION_IDX";
     List<Question> questionList;
     int curQuesitonIdx = 0;
     TextView tvContent;
@@ -30,9 +34,51 @@ public class MainActivity extends AppCompatActivity {
 
         generateQuestions();
 
+        if(savedInstanceState != null) {
+            curQuesitonIdx = savedInstanceState.getInt(SAVED_CUR_QUESTION_IDX, 0);
+        }
+
         ShowCurQuestion();
 
         setContentView(view);
+
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SAVED_CUR_QUESTION_IDX, curQuesitonIdx);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 
     private void ShowCurQuestion() {
